524cdf5ce2ffc8fb5bef04978a5474b5f4328472
Saša Barišić <cartman300@net.hr>
2017-03-09 02:16:16 +0100

    Project updates

--- gravity/unistd.h
+++ gravity/unistd.h
@@ -1,8 +1,11 @@
 #pragma once
+#pragma comment(lib, "Shlwapi.lib")
 
 #include <basetsd.h>
 #include <io.h>
 #include <stdio.h>
+
+#define bzero(b, len) memset((b), 0, (len))
 
 typedef SSIZE_T ssize_t;
 typedef int mode_t;
