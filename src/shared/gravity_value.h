9bcc06bde5fdd4cfb55f763e9682b16a477fe03d
Marco Bambini <marco@creolabs.com>
2018-05-22 18:11:45 +0200

    Raised version to 0.4.8

--- src/shared/gravity_value.h
+++ src/shared/gravity_value.h
@@ -66,8 +66,8 @@
 extern "C" {
 #endif
 
-#define GRAVITY_VERSION						"0.4.7"     // git tag 0.4.7
-#define GRAVITY_VERSION_NUMBER				0x000407    // git push --tags
+#define GRAVITY_VERSION						"0.4.8"     // git tag 0.4.8
+#define GRAVITY_VERSION_NUMBER				0x000408    // git push --tags
 #define GRAVITY_BUILD_DATE					__DATE__
 
 #ifndef GRAVITY_ENABLE_DOUBLE
