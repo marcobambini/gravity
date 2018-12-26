dac82ba29fe9cdb6d1666901f763fc424ffa27ac
Marco Bambini <marco@creolabs.com>
2018-12-26 15:51:03 +0100

    Added a way to automatically call user implemented instance free

--- src/shared/gravity_value.h
+++ src/shared/gravity_value.h
@@ -66,8 +66,8 @@
 extern "C" {
 #endif
 
-#define GRAVITY_VERSION						"0.6.1"     // git tag 0.6.1
-#define GRAVITY_VERSION_NUMBER				0x000601    // git push --tags
+#define GRAVITY_VERSION						"0.6.2"     // git tag 0.6.2
+#define GRAVITY_VERSION_NUMBER				0x000602    // git push --tags
 #define GRAVITY_BUILD_DATE                  __DATE__
 
 #ifndef GRAVITY_ENABLE_DOUBLE
