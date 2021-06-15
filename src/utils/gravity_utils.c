18690d35f4d2638f94f13f5c71b837b7f318166d
tDwtp <Dark-player@gmx.de>
2021-06-15 04:20:55 +0200

    fix read size different from file size due to line-ending
    fix line ending causing fsize != fsize2 in gravity_utils.d/file_read() due to not reading in binary mode.

--- src/utils/gravity_utils.c
+++ src/utils/gravity_utils.c
@@ -135,7 +135,7 @@ char *file_read(const char *path, size_t *len) {
     fsize = (off_t) file_size(path);
     if (fsize < 0) goto abort_read;
     
-    fd = open(path, O_RDONLY);
+    fd = open(path, O_RDONLY|O_BINARY);
     if (fd < 0) goto abort_read;
     
     buffer = (char *)mem_alloc(NULL, (size_t)fsize + 1);
