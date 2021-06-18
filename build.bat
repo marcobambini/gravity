616aec046d85cf89f82ef6e82b9c29806ef347bd
tDwtp <Dark-player@gmx.de>
2021-06-18 16:51:20 +0200

    build batch-file for simple building (msbuild/cmake required to be in path)
    Requires msbuild and cmake (which comes with msbuild now)

---
+++ build.bat
@@ -0,0 +1,40 @@
+@IF EXIST bld RMDIR /S /Q bld
+cmake -Bbld .
+@PUSHD bld
+@FOR %%X IN (msbuild.exe) DO @IF NOT "%%~$PATH:X" == "" @CALL :msvc
+@POPD
+@GOTO:EOF
+
+:msvc
+msbuild gravity.sln /p:Configuration=Debug
+msbuild gravity.sln /p:Configuration=Release
+MKDIR Debug\bin
+MKDIR Debug\bin\lib
+MKDIR Debug\lib
+MOVE bin\Debug\gravity.exe Debug\bin\gravity.exe
+MOVE bin\Debug\gravity.pdb Debug\bin\gravity.pdb
+MOVE lib\Debug\gravityapi.lib Debug\bin\lib\gravityapi.lib
+MOVE lib\Debug\gravityapi.exp Debug\bin\lib\gravityapi.exp
+MOVE src\Debug\gravityapi.dll Debug\bin\gravityapi.dll
+MOVE src\Debug\gravityapi.pdb Debug\bin\gravityapi.pdb
+MOVE lib\Debug\gravityapi_s.lib Debug\lib\gravityapi_s.lib
+MOVE lib\Debug\gravityapi_s.exp Debug\lib\gravityapi_s.exp
+MOVE lib\Debug\gravityapi_s.pdb Debug\lib\gravityapi_s.pdb
+RMDIR /S /Q bin\Debug
+RMDIR /S /Q lib\Debug
+RMDIR /S /Q src\Debug
+
+
+MKDIR Release\bin
+MKDIR Release\bin\lib
+MKDIR Release\lib
+MOVE bin\Release\gravity.exe Release\bin\gravity.exe
+MOVE lib\Release\gravity.exp Release\bin\lib\gravity.exp
+MOVE src\Release\gravityapi.dll Release\bin\gravityapi.dll
+MOVE lib\Release\gravityapi.lib Release\bin\lib\gravityapi.lib
+MOVE lib\Release\gravityapi.exp Release\bin\lib\gravityapi.exp
+MOVE lib\Release\gravityapi_s.lib Release\lib\gravityapi_s.lib
+MOVE lib\Release\gravityapi_s.exp Release\lib\gravityapi_s.exp
+RMDIR /S /Q bin\Release
+RMDIR /S /Q lib\Release
+RMDIR /S /Q src\Release
